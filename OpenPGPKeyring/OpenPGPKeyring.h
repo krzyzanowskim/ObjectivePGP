212b9c231dd8fa90af2e98b93162c2efae3e0ff3
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2014-05-03 10:19:40 +0200

    Initial Commit

---
+++ OpenPGPKeyring/OpenPGPKeyring.h
@@ -0,0 +1,13 @@
+//
+//  OpenPGPKeyring.h
+//  OpenPGPKeyring
+//
+//  Created by Marcin Krzyzanowski on 03/05/14.
+//  Copyright (c) 2014 Marcin Krzyżanowski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OpenPGPKeyring : NSObject
+
+@end
---
+++ OpenPGPKeyring/OpenPGPKeyring.m
@@ -0,0 +1,13 @@
+//
+//  OpenPGPKeyring.m
+//  OpenPGPKeyring
+//
+//  Created by Marcin Krzyzanowski on 03/05/14.
+//  Copyright (c) 2014 Marcin Krzyżanowski. All rights reserved.
+//
+
+#import "OpenPGPKeyring.h"
+
+@implementation OpenPGPKeyring
+
+@end
