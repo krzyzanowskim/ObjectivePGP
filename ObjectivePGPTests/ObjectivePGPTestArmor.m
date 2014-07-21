660546b2f61e5bb7a02bab5b3f3fdd3fec725cb4
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2014-07-21 22:50:51 +0200

    test embeded armor

--- ObjectivePGPTests/ObjectivePGPTestArmor.m
+++ ObjectivePGPTests/ObjectivePGPTestArmor.m
@@ -81,4 +81,36 @@
     XCTAssertEqualObjects(decodedData, keyData);
 }
 
+- (void) testEmbededArmoredData
+{
+    [self.oPGP importKeysFromFile:self.pubKeyringPath];
+
+    PGPKey *key = self.oPGP.keys[0];
+
+    NSError *exportError = nil;
+    NSData *keyData = [key export:&exportError];
+    XCTAssertNil(exportError);
+    XCTAssertNotNil(keyData);
+    
+    NSData *armoredData = [PGPArmor armoredData:keyData as:PGPArmorTypePublicKey];
+    XCTAssertNotNil(armoredData);
+
+    NSFileHandle *fileHandle = [NSFileHandle fileHandleForWritingAtPath:[self.workingDirectory stringByAppendingPathComponent:@"pubkey.asc"]];
+    [fileHandle seekToEndOfFile];
+    [fileHandle writeData:[@"some buzzzzzzzz\n" dataUsingEncoding:NSUTF8StringEncoding]];
+    [fileHandle writeData:armoredData];
+    [fileHandle writeData:[@"\nmore buzzz\n" dataUsingEncoding:NSUTF8StringEncoding]];
+    [fileHandle closeFile];
+
+    NSError *loadError = nil;
+    NSString *armoredString = [NSString stringWithContentsOfFile:[self.workingDirectory stringByAppendingPathComponent:@"pubkey.asc"] encoding:NSASCIIStringEncoding error:&loadError];
+    XCTAssertNil(loadError);
+    XCTAssertNotNil(armoredString);
+
+    NSError *readArmoredError = nil;
+    NSData *decodedData = [PGPArmor readArmoredData:armoredString error:&readArmoredError];
+    XCTAssertNil(readArmoredError);
+    XCTAssertNotNil(decodedData);
+}
+
 @end
