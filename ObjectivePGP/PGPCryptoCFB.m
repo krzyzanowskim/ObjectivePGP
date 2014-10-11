8c857388e4d3b263652e827a3b04e46a972ef566
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2014-10-12 00:12:12 +0200

    minor update to AES ecnryption code.

--- ObjectivePGP/PGPCryptoCFB.m
+++ ObjectivePGP/PGPCryptoCFB.m
@@ -80,14 +80,18 @@
         case PGPSymmetricAES192:
         case PGPSymmetricAES256:
         {
-            AES_KEY encrypt_key;
-            AES_set_encrypt_key(sessionKeyData.bytes, (unsigned int)keySize * 8, &encrypt_key);
+            AES_KEY aes_key;
+            if (decrypt) {
+                AES_set_decrypt_key(sessionKeyData.bytes, (unsigned int)keySize * 8, &aes_key);
+            } else {
+                AES_set_encrypt_key(sessionKeyData.bytes, (unsigned int)keySize * 8, &aes_key);
+            }
             
             int num = 0;
-            AES_cfb128_encrypt(encryptedBytes, outBuffer, outBufferLength, &encrypt_key, iv, &num, decrypt ? AES_DECRYPT : AES_ENCRYPT);
+            AES_cfb128_encrypt(encryptedBytes, outBuffer, outBufferLength, &aes_key, iv, &num, decrypt ? AES_DECRYPT : AES_ENCRYPT);
             decryptedData = [NSData dataWithBytes:outBuffer length:outBufferLength];
             
-            memset(&encrypt_key, 0, sizeof(AES_KEY));
+            memset(&aes_key, 0, sizeof(AES_KEY));
         }
             break;
         case PGPSymmetricIDEA:
