fde21c21bc90687b645426af98422a8e3d8dfe81
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2017-05-14 22:25:57 +0200

    Add missing file

---
+++ ObjectivePGP/PGPLogging.h
@@ -0,0 +1,23 @@
+//
+//  PGPLogging.h
+//  ObjectivePGP
+//
+//  Created by Marcin Krzyzanowski on 14/05/2017.
+//  Copyright © 2017 Marcin Krzyżanowski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+#define PGPLogMacro(_level, _tag, _message) \
+    NSLog(@"[%s] %@ %s/%tu %@",_tag, @(_level), __PRETTY_FUNCTION__, __LINE__, _message())
+
+#ifdef DEBUG
+    #define PGPLogDebug(format, ...) PGPLogMacro(0, "ObjectivePGP", (^{ return [NSString stringWithFormat:(@"" format), ##__VA_ARGS__]; }))
+#else
+    #define PGPLogDebug(format, ...)
+#endif
+
+
+NS_ASSUME_NONNULL_END
