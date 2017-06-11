9dd4012f02eb77ca846738db4bdc2628929ce424
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2017-06-11 02:13:59 -0700

    Public headers for all targets

---
+++ ObjectivePGP/PGPCompoundKey+Private.h
@@ -0,0 +1,13 @@
+//
+//  PGPCompoundKey+Private.h
+//  ObjectivePGP
+//
+//  Created by Marcin Krzyzanowski on 10/06/2017.
+//  Copyright © 2017 Marcin Krzyżanowski. All rights reserved.
+//
+
+#ifndef PGPCompoundKey_Private_h
+#define PGPCompoundKey_Private_h
+
+
+#endif /* PGPCompoundKey_Private_h */
