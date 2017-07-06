67a4baed84e997d1225163672af6c6b39b2c6c23
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2017-07-06 22:41:51 +0200

    Playground sample

--- ObjectivePGP.playground/Contents.swift
+++ ObjectivePGP.playground/Contents.swift
@@ -1,7 +1,17 @@
-//: Playground - noun: a place where people can play
-
-import Cocoa
-
-// Remember to build ObjectivePGP.framework target
+// Build ObjectivePGP.framework target first.
 import ObjectivePGP
+import Foundation
+
+guard let keyURL = Bundle.main.url(forResource: "marcin.krzyzanowski@gmail.com", withExtension: "asc"),
+      let keyData = try? Data(contentsOf: keyURL) else { fatalError("Can't find key file") }
+
+let pgp = ObjectivePGP()
+pgp.importKeys(from: keyData)
+guard let key = pgp.findKey(forIdentifier: "878ECFB866753341") else { fatalError("Can't find the key") }
+
+// Encrypt 5 bytes using selected key
+let encryptedArmoredData = try! pgp.encryptData(Data(bytes: [1,2,3,4,5]), using: [key], armored: true)
+let encryptedAscii = String(data: encryptedArmoredData, encoding: .utf8)!
+
+print(encryptedAscii)
 
