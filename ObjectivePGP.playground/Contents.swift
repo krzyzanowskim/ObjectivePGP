012827cbd7dd25567e4776449b5e8ae36f2f1d37
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2017-11-04 22:18:29 +0100

    Update playground

--- ObjectivePGP.playground/Contents.swift
+++ ObjectivePGP.playground/Contents.swift
@@ -2,19 +2,12 @@
 import ObjectivePGP
 import Foundation
 
-let pgp = ObjectivePGP()
-
 // Generate new key
-let newKey = KeyGenerator().generate(for: "marcin@krzyzanowskim.com", passphrase: nil)
-
-// Import key
-pgp.import(keys: [newKey])
-
-// Search imported key
-guard let key = pgp.findKey(newKey.keyID) else { fatalError("Can't find the key") }
+let key1 = KeyGenerator().generate(for: "marcin@krzyzanowskim.com", passphrase: nil)
+let key2 = KeyGenerator().generate(for: "fran@krzyzanowskim.com", passphrase: nil)
 
 // Encrypt 5 bytes using selected key
-let encryptedArmoredData = try! pgp.encrypt(Data(bytes: [1,2,3,4,5]), using: [key], armored: true)
+let encryptedArmoredData = try ObjectivePGP.encrypt(Data(bytes: [1,2,3,4,5]), using: [key1, key2], armored: true)
 let encryptedAscii = String(data: encryptedArmoredData, encoding: .utf8)
 
 print(encryptedAscii ?? "Missing")
