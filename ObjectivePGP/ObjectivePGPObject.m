fba91d61ac3e5328b1558b032fae948b2901bafe
Oliver Wiese <oliver.wiese@fu-berlin.de>
2018-01-06 10:39:25 +0100

    clean code.

--- ObjectivePGP/ObjectivePGPObject.m
+++ ObjectivePGP/ObjectivePGPObject.m
@@ -277,16 +277,13 @@ NS_ASSUME_NONNULL_BEGIN
     NSData *content;
     if (shouldSign) {
         // sign data if requested
-        // sign data if requested
         let signKeys = [NSMutableArray<PGPKey *> array];
         for (PGPKey* key in keys){
             if (key.secretKey && key.signingSecretKey != NULL){
                 [signKeys addObject:key];
             }
-            
         }
         content = [self sign:dataToEncrypt detached:NO usingKeys:signKeys passphraseForKey:passphraseForKeyBlock error:error];
-
     } else {
         // Prepare literal packet
         let literalPacket = [PGPLiteralPacket literalPacket:PGPLiteralPacketBinary withData:dataToEncrypt];
