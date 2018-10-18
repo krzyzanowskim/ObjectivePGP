fd475af1125990d55a499605a528a0aec7656006
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2018-10-18 21:31:23 +0200

    Release 0.13.0

--- Frameworks/macosx/ObjectivePGP.framework/Versions/A/PrivateHeaders/PGPUser+Private.h
+++ Frameworks/macosx/ObjectivePGP.framework/Versions/A/PrivateHeaders/PGPUser+Private.h
@@ -25,7 +25,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (instancetype)initWithUserIDPacket:(PGPUserIDPacket *)userPacket NS_DESIGNATED_INITIALIZER;
 
-- (nullable PGPSignaturePacket *)validSelfCertificate;
+- (nullable PGPSignaturePacket *)latestSelfCertificate;
 
 @end
 
