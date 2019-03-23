95e47057f70639c70f1405f650de8874a4fe8643
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2019-03-23 19:53:10 +0100

    Update OpenSSL scripts

--- scripts/build-openssl.sh
+++ scripts/build-openssl.sh
@@ -1,69 +1,62 @@
 #!/usr/bin/env bash
 
 BASE_PWD="$PWD"
-SCRIPT_DIR="$(dirname "$0")"
-VENDOR_DIR="${SCRIPT_DIR}/../vendor"
-LIBS_DIR="${VENDOR_DIR}/libs"
-SRC_DIR="${VENDOR_DIR}/openssl-src"
-BUILD_DIR="${VENDOR_DIR}/build"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-rm -rf "${BUILD_DIR}"
-rm -rf "${SRC_DIR}"
+TMP_DIR=$( mktemp -d )
+OPENSSL_SRC_DIR=${TMP_DIR}/src
+OPENSSL_BUILD_DIR=${TMP_DIR}/build
+LIBS_DIR=$( realpath ${SCRIPT_DIR}/../vendor/libs )
 
-curl -Lk https://www.openssl.org/source/openssl-1.1.1.tar.gz -o "${VENDOR_DIR}/openssl-src.tar.gz"
-tar xvzf "${VENDOR_DIR}/openssl-src.tar.gz" -C "${VENDOR_DIR}/"
-rm -f "${VENDOR_DIR}/openssl-src.tar.gz"
-mv -f "${VENDOR_DIR}/openssl-1.1.1" "${SRC_DIR}"
+mkdir -p ${OPENSSL_SRC_DIR}
+mkdir -p ${OPENSSL_BUILD_DIR}
+mkdir -p ${LIBS_DIR}
 
-mkdir -p "${BUILD_DIR}/iphoneos_armv7"
-cd "${BUILD_DIR}/iphoneos_armv7"
-../../openssl-src/Configure no-shared no-hw no-zlib-dynamic no-asm no-dso no-engine no-hw no-ssl2 no-ssl3 ios-xcrun
+curl -Lk https://www.openssl.org/source/openssl-1.1.1b.tar.gz | tar -xzp --strip-components=1 -C ${OPENSSL_SRC_DIR}
+
+mkdir -p "${OPENSSL_BUILD_DIR}/iphoneos_armv7"
+cd "${OPENSSL_BUILD_DIR}/iphoneos_armv7"
+${OPENSSL_SRC_DIR}/Configure no-shared no-hw no-zlib-dynamic no-asm no-dso no-engine no-hw no-ssl2 no-ssl3 ios-xcrun
 make -j 8
 cd "${BASE_PWD}"
 
-mkdir -p "${BUILD_DIR}/iphoneos_arm64"
-cd "${BUILD_DIR}/iphoneos_arm64"
-../../openssl-src/Configure no-shared no-hw no-zlib-dynamic no-asm no-dso no-engine no-hw no-ssl2 no-ssl3 ios64-xcrun
+mkdir -p "${OPENSSL_BUILD_DIR}/iphoneos_arm64"
+cd "${OPENSSL_BUILD_DIR}/iphoneos_arm64"
+${OPENSSL_SRC_DIR}/Configure no-shared no-hw no-zlib-dynamic no-asm no-dso no-engine no-hw no-ssl2 no-ssl3 ios64-xcrun
 make -j 8
 
-cd "${BASE_PWD}"
-mkdir -p "${BUILD_DIR}/iossimulator"
-cd "${BUILD_DIR}/iossimulator"
-../../openssl-src/Configure no-shared no-hw no-zlib-dynamic no-asm no-dso no-engine no-hw no-ssl2 no-ssl3 iossimulator-xcrun
+mkdir -p "${OPENSSL_BUILD_DIR}/iossimulator"
+cd "${OPENSSL_BUILD_DIR}/iossimulator"
+${OPENSSL_SRC_DIR}/Configure no-shared no-hw no-zlib-dynamic no-asm no-dso no-engine no-hw no-ssl2 no-ssl3 iossimulator-xcrun
 make -j 8
 
-cd "${BASE_PWD}"
-mkdir -p "${BUILD_DIR}/macosx"
-cd "${BUILD_DIR}/macosx"
-../../openssl-src/Configure no-shared no-hw no-zlib-dynamic no-asm no-dso no-engine no-ssl2 no-ssl3 enable-ec_nistp_64_gcc_128 -mmacosx-version-min=10.9 darwin64-x86_64-cc
+mkdir -p "${OPENSSL_BUILD_DIR}/macosx"
+cd "${OPENSSL_BUILD_DIR}/macosx"
+${OPENSSL_SRC_DIR}/Configure no-shared no-hw no-zlib-dynamic no-asm no-dso no-engine no-ssl2 no-ssl3 enable-ec_nistp_64_gcc_128 -mmacosx-version-min=10.9 darwin64-x86_64-cc
 make -j 8
-
-cd "${BASE_PWD}"
 
 lipo -create \
-     "${BUILD_DIR}/iphoneos_arm64/libcrypto.a" \
-     "${BUILD_DIR}/iphoneos_armv7/libcrypto.a" \
-     "${BUILD_DIR}/iossimulator/libcrypto.a" \
+     "${OPENSSL_BUILD_DIR}/iphoneos_arm64/libcrypto.a" \
+     "${OPENSSL_BUILD_DIR}/iphoneos_armv7/libcrypto.a" \
+     "${OPENSSL_BUILD_DIR}/iossimulator/libcrypto.a" \
      -o "${LIBS_DIR}/ios/libcrypto.a"
 
 lipo -create \
-     "${BUILD_DIR}/macosx/libcrypto.a" \
+     "${OPENSSL_BUILD_DIR}/macosx/libcrypto.a" \
      -o "${LIBS_DIR}/macosx/libcrypto.a"
 
 rm -rf "${LIBS_DIR}/ios/openssl/"
 mkdir -p "${LIBS_DIR}/ios/openssl"
-cp -R "${SRC_DIR}"/include/openssl/*.h "${LIBS_DIR}/ios/openssl/"
-cp -R "${BUILD_DIR}"/iphoneos_arm64/include/openssl/*.h "${LIBS_DIR}/ios/openssl"
+cp -R "${OPENSSL_SRC_DIR}"/include/openssl/*.h "${LIBS_DIR}/ios/openssl/"
+cp -R "${OPENSSL_BUILD_DIR}"/iphoneos_arm64/include/openssl/*.h "${LIBS_DIR}/ios/openssl"
 
 rm -rf "${LIBS_DIR}/macosx/openssl/"
 mkdir -p "${LIBS_DIR}/macosx/openssl"
-cp -R "${SRC_DIR}"/include/openssl/*.h "${LIBS_DIR}/macosx/openssl"
-cp -R "${BUILD_DIR}"/macosx/include/openssl/*.h "${LIBS_DIR}/macosx/openssl"
+cp -R "${OPENSSL_SRC_DIR}"/include/openssl/*.h "${LIBS_DIR}/macosx/openssl"
+cp -R "${OPENSSL_BUILD_DIR}"/macosx/include/openssl/*.h "${LIBS_DIR}/macosx/openssl"
 
 # Update list of exported symbols
 # The file is later used by the UNEXPORTED_SYMBOLS_FILE to hide the symbols
-xcrun nm -gUj "${LIBS_DIR}/ios/libcrypto.a" | grep -v "^$" | grep "^_" > "${LIBS_DIR}/ios/symbols"
-xcrun nm -gUj "${LIBS_DIR}/macosx/libcrypto.a" | grep -v "^$" | grep "^_" > "${LIBS_DIR}/macosx/symbols"
+${LIBS_DIR}/export-symbols.sh
 
-rm -rf "${BUILD_DIR}"
-rm -rf "${SRC_DIR}"
+rm -rf ${TMP_DIR}
--- vendor/libs/export-symbols.sh
+++ vendor/libs/export-symbols.sh
@@ -1,7 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Export global symbols to the file.
 # The file is later used by the UNEXPORTED_SYMBOLS_FILE to hide the symbols
 
-xcrun nm -gUj macosx/libcrypto.a | grep -v "^$" | grep "^_" > macosx/symbols
-xcrun nm -gUj ios/libcrypto.a | grep -v "^$" | grep "^_" > ios/symbols
+BASE_PWD="$PWD"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+LIBS_DIR=$( realpath ${SCRIPT_DIR}/../../vendor/libs )
+
+xcrun nm -gUj "${LIBS_DIR}/ios/libcrypto.a" | grep -v "^$" | grep "^_" > "${LIBS_DIR}/ios/symbols"
+xcrun nm -gUj "${LIBS_DIR}/macosx/libcrypto.a" | grep -v "^$" | grep "^_" > "${LIBS_DIR}/macosx/symbols"
