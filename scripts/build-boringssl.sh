c0b664e972f289d410ad3c8ab72fe4d5d9467d3d
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2019-12-01 18:44:27 +0100

    Update BoringSSL script

--- scripts/build-boringssl.sh
+++ scripts/build-boringssl.sh
@@ -6,61 +6,65 @@ BASE_PWD="$PWD"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 TMP_DIR=$( mktemp -d )
+
+# chromium-stable branch https://github.com/google/boringssl/commits/chromium-stable
+BORINGSSL_COMMIT="05cd93068b0a553afc48f69acbceae10c6a17593"
 BORINGSSL_SRC_DIR="${TMP_DIR}/src/boringssl.googlesource.com/boringssl"
 BORINGSSL_BUILD_DIR="${TMP_DIR}/build"
-LIBS_DIR="${SCRIPT_DIR}/../vendor/libs"
+VENDOR_DIR="${SCRIPT_DIR}/../vendor"
+LIBS_DIR="${VENDOR_DIR}/BoringSSL"
 CMAKE_OPTIONS="-GNinja -DCMAKE_BUILD_TYPE=Release -H${BORINGSSL_SRC_DIR} -Wno-dev"
-SYMBOLS_PREFIX="pgp_boringssl"
+SYMBOLS_PREFIX="pgp"
 CMAKE_PREFIX_OPTIONS="-DBORINGSSL_PREFIX=${SYMBOLS_PREFIX}"
 
 export GOPATH="${TMP_DIR}"
 
-PGPBORINGSSL_MODULE_HEADERS=("${SYMBOLS_PREFIX}_aes.h"
-                             "${SYMBOLS_PREFIX}_arm_arch.h"
-                             "${SYMBOLS_PREFIX}_asn1_mac.h"
-                             "${SYMBOLS_PREFIX}_asn1t.h"
-                             "${SYMBOLS_PREFIX}_base.h"
-                             "${SYMBOLS_PREFIX}_bio.h"
-                             "${SYMBOLS_PREFIX}_blowfish.h"
-                             "${SYMBOLS_PREFIX}_boringssl_prefix_symbols.h"
-                             "${SYMBOLS_PREFIX}_boringssl_prefix_symbols_asm.h"
-                             "${SYMBOLS_PREFIX}_cast.h"
-                             "${SYMBOLS_PREFIX}_chacha.h"
-                             "${SYMBOLS_PREFIX}_cmac.h"
-                             "${SYMBOLS_PREFIX}_conf.h"
-                             "${SYMBOLS_PREFIX}_cpu.h"
-                             "${SYMBOLS_PREFIX}_curve25519.h"
-                             "${SYMBOLS_PREFIX}_des.h"
-                             "${SYMBOLS_PREFIX}_dtls1.h"
-                             "${SYMBOLS_PREFIX}_e_os2.h"
-                             "${SYMBOLS_PREFIX}_ec.h"
-                             "${SYMBOLS_PREFIX}_ec_key.h"
-                             "${SYMBOLS_PREFIX}_ecdsa.h"
-                             "${SYMBOLS_PREFIX}_err.h"
-                             "${SYMBOLS_PREFIX}_evp.h"
-                             "${SYMBOLS_PREFIX}_hkdf.h"
-                             "${SYMBOLS_PREFIX}_hmac.h"
-                             "${SYMBOLS_PREFIX}_hrss.h"
-                             "${SYMBOLS_PREFIX}_md4.h"
-                             "${SYMBOLS_PREFIX}_md5.h"
-                             "${SYMBOLS_PREFIX}_obj_mac.h"
-                             "${SYMBOLS_PREFIX}_objects.h"
-                             "${SYMBOLS_PREFIX}_opensslv.h"
-                             "${SYMBOLS_PREFIX}_ossl_typ.h"
-                             "${SYMBOLS_PREFIX}_pkcs12.h"
-                             "${SYMBOLS_PREFIX}_poly1305.h"
-                             "${SYMBOLS_PREFIX}_rand.h"
-                             "${SYMBOLS_PREFIX}_rc4.h"
-                             "${SYMBOLS_PREFIX}_ripemd.h"
-                             "${SYMBOLS_PREFIX}_rsa.h"
-                             "${SYMBOLS_PREFIX}_safestack.h"
-                             "${SYMBOLS_PREFIX}_sha.h"
-                             "${SYMBOLS_PREFIX}_siphash.h"
-                             "${SYMBOLS_PREFIX}_srtp.h"
-                             "${SYMBOLS_PREFIX}_ssl.h"
-                             "${SYMBOLS_PREFIX}_x509v3.h")
-
-echo $TMP_DIR
+PGPBORINGSSL_MODULE_HEADERS=("aes.h"
+                             "arm_arch.h"
+                             "asn1_mac.h"
+                             "asn1t.h"
+                             "base.h"
+                             "bio.h"
+                             "blowfish.h"
+                             "boringssl_prefix_symbols.h"
+                             "boringssl_prefix_symbols_asm.h"
+                             "cast.h"
+                             "chacha.h"
+                             "cmac.h"
+                             "conf.h"
+                             "cpu.h"
+                             "curve25519.h"
+                             "des.h"
+                             "dsa.h"
+                             "dh.h"
+                             "dtls1.h"
+                             "e_os2.h"
+                             "ec.h"
+                             "ec_key.h"
+                             "ecdsa.h"
+                             "err.h"
+                             "evp.h"
+                             "hkdf.h"
+                             "hmac.h"
+                             "hrss.h"
+                             "md4.h"
+                             "md5.h"
+                             "obj_mac.h"
+                             "objects.h"
+                             "opensslv.h"
+                             "ossl_typ.h"
+                             "pkcs12.h"
+                             "poly1305.h"
+                             "rand.h"
+                             "rc4.h"
+                             "ripemd.h"
+                             "rsa.h"
+                             "safestack.h"
+                             "sha.h"
+                             "siphash.h"
+                             "srtp.h"
+                             "ssl.h"
+                             "x509v3.h")
 
 if ! hash gsed 2>/dev/null; then
     echo "You need sed \"gsed\" to run this script ..."
@@ -72,20 +76,21 @@ fi
 mkdir -p ${BORINGSSL_SRC_DIR}
 mkdir -p ${BORINGSSL_BUILD_DIR}
 
-rm -rf ${LIBS_DIR}
-mkdir -p ${LIBS_DIR}/{ios,macosx}
-
-if [ ! -r /tmp/chromium-stable.zip ]; then
-  echo "Downloading..."
-  curl -L https://github.com/google/boringssl/archive/chromium-stable.zip > /tmp/chromium-stable.zip
+if [ ! -r /tmp/boringssl-${BORINGSSL_COMMIT}.zip ]; then
+  echo "Downloading /tmp/boringssl-${BORINGSSL_COMMIT}.zip"
+  curl -L https://github.com/google/boringssl/archive/${BORINGSSL_COMMIT}.zip > /tmp/boringssl-${BORINGSSL_COMMIT}.zip
 fi
 
-echo "Expanding..."
-tar -xzp --strip-components=1 -f /tmp/chromium-stable.zip -C ${BORINGSSL_SRC_DIR}
+echo "Expanding to ${BORINGSSL_SRC_DIR} ..."
+tar -xzp --strip-components=1 -f /tmp/boringssl-${BORINGSSL_COMMIT}.zip -C ${BORINGSSL_SRC_DIR}
+
+rm -rf ${LIBS_DIR}/{ios,macosx}
+git checkout '**/.keep'
+mkdir -p ${LIBS_DIR}/{ios,macosx}/lib
 
 ################## MACOSX ##################
 
-PLATFORM_LIBS_DIR="${LIBS_DIR}/macosx"
+PLATFORM_LIBS_DIR="${LIBS_DIR}/macosx/lib"
 
 # Build generic for macOS
 cmake -DCMAKE_OSX_SYSROOT=macosx ${CMAKE_OPTIONS} -DCMAKE_OSX_DEPLOYMENT_TARGET="10.11" -DCMAKE_OSX_ARCHITECTURES=x86_64 -B"${BORINGSSL_BUILD_DIR}/macosx"
@@ -108,25 +113,26 @@ lipo -create \
 
 # Update list of exported symbols
 # The file is later used by the UNEXPORTED_SYMBOLS_FILE to hide the symbols
-xcrun nm -gUj "${PLATFORM_LIBS_DIR}/libcrypto.a" | grep -v "^$" | grep "^_" > "${PLATFORM_LIBS_DIR}/symbols"
+# xcrun nm -gUj "${PLATFORM_LIBS_DIR}/libcrypto.a" | grep -v "^$" | grep "^_" > "${PLATFORM_LIBS_DIR}/symbols"
 
 # Prefixed headers
-mkdir -p "${PLATFORM_LIBS_DIR}/PGPBoringSSL"
-cp ${BORINGSSL_SRC_DIR}/include/openssl/* "${PLATFORM_LIBS_DIR}/PGPBoringSSL"
-go run ${BORINGSSL_SRC_DIR}/util/make_prefix_headers.go -out ${PLATFORM_LIBS_DIR}/PGPBoringSSL "${PLATFORM_LIBS_DIR}/symbols"
-chmod 444 ${PLATFORM_LIBS_DIR}/PGPBoringSSL/*
+mkdir -p "${PLATFORM_LIBS_DIR}/../PGPBoringSSL"
+cp ${BORINGSSL_SRC_DIR}/include/openssl/* "${PLATFORM_LIBS_DIR}/../PGPBoringSSL"
+go run ${BORINGSSL_SRC_DIR}/util/make_prefix_headers.go -out "${PLATFORM_LIBS_DIR}/../PGPBoringSSL" "${TMP_DIR}/symbols-macosx.txt"
+chmod 444 ${PLATFORM_LIBS_DIR}/../PGPBoringSSL/*
 
-pushd ${PLATFORM_LIBS_DIR}/PGPBoringSSL
+pushd ${PLATFORM_LIBS_DIR}/../PGPBoringSSL
 # Now change the imports from "<PGPBoringSSL/X> to "<SYMBOLS_PREFIX_X>", apply the same prefix to the 'boringssl_prefix_symbols' headers.
-find . -name "*.[ch]" -or -name "*.cc" -or -name "*.S" | xargs gsed -i -e 's+include <openssl/+include <'"${SYMBOLS_PREFIX}"'_+' -e 's+include <boringssl_prefix_symbols+include '"<${SYMBOLS_PREFIX}"'_boringssl_prefix_symbols+'
+# find . -name "*.[ch]" -or -name "*.cc" -or -name "*.S" | xargs gsed -i -e 's+include <openssl/+include <'"${SYMBOLS_PREFIX}"'_+' -e 's+include <boringssl_prefix_symbols+include '"<${SYMBOLS_PREFIX}"'_boringssl_prefix_symbols+'
 # Okay now we need to rename the headers adding the prefix "${SYMBOLS_PREFIX}_".
-find . -name "*.h" | gsed -e "s_./__" | xargs -I {} mv {} "${SYMBOLS_PREFIX}_{}"
+# find . -name "*.h" | gsed -e "s_./__" | xargs -I {} mv {} "${SYMBOLS_PREFIX}_{}"
 # Finally, make sure we refer to them by their prefixed names, and change any includes from angle brackets to quotation marks.
-find . -name "*.h" | xargs gsed -i -e 's/include "/include "'"${SYMBOLS_PREFIX}"'_/' -e 's/include <'"${SYMBOLS_PREFIX}"'_\(.*\)>/include "'"${SYMBOLS_PREFIX}"'_\1"/'
+# find . -name "*.h" | xargs gsed -i -e 's/include "/include "'"${SYMBOLS_PREFIX}"'_/' -e 's/include <'"${SYMBOLS_PREFIX}"'_\(.*\)>/include "'"${SYMBOLS_PREFIX}"'_\1"/'
+
 # We need BoringSSL to be modularised
 echo "MODULARISING BoringSSL"
 
-cat << EOF > "${PLATFORM_LIBS_DIR}/PGPBoringSSL/PGPBoringSSL.h"
+cat << EOF > "${PLATFORM_LIBS_DIR}/../PGPBoringSSL/PGPBoringSSL.h"
 //===----------------------------------------------------------------------===//
 // This source file is part of the ObjectivePGP project
 //===----------------------------------------------------------------------===//
@@ -136,17 +142,19 @@ cat << EOF > "${PLATFORM_LIBS_DIR}/PGPBoringSSL/PGPBoringSSL.h"
 EOF
 
 for h in ${PGPBORINGSSL_MODULE_HEADERS[@]}; do
-  echo  "#include \"$h\"" >> ${PLATFORM_LIBS_DIR}/PGPBoringSSL/PGPBoringSSL.h
+  echo  "#include \"$h\"" >> ${PLATFORM_LIBS_DIR}/../PGPBoringSSL/PGPBoringSSL.h
 done
 
-echo "#endif  // PGP_BORINGSSL_H" >> "${PLATFORM_LIBS_DIR}/PGPBoringSSL/PGPBoringSSL.h"
+echo "#endif  // PGP_BORINGSSL_H" >> "${PLATFORM_LIBS_DIR}/../PGPBoringSSL/PGPBoringSSL.h"
 popd
 
 rm -rf "${BORINGSSL_BUILD_DIR}"
 
 ################## iOS ##################
 
-PLATFORM_LIBS_DIR="${LIBS_DIR}/ios"
+PLATFORM_LIBS_DIR="${LIBS_DIR}/ios/lib"
+
+mkdir -p ${BORINGSSL_BUILD_DIR}
 
 # Build generic for iOS
 cmake -DCMAKE_OSX_SYSROOT=iphoneos ${CMAKE_OPTIONS} -DCMAKE_OSX_DEPLOYMENT_TARGET="8.0" -DCMAKE_OSX_ARCHITECTURES=arm64 -B"${BORINGSSL_BUILD_DIR}/iphoneos_arm64"
@@ -176,29 +184,27 @@ lipo -create \
      "${BORINGSSL_BUILD_DIR}/iphonesimulator_x86_64/crypto/libcrypto.a" \
      -o "${PLATFORM_LIBS_DIR}/libcrypto.a"
 
-cp -R "${BORINGSSL_SRC_DIR}/include/" "${PLATFORM_LIBS_DIR}/"
-
 # Update list of exported symbols
 # The file is later used by the UNEXPORTED_SYMBOLS_FILE to hide the symbols
-xcrun nm -gUj "${PLATFORM_LIBS_DIR}/libcrypto.a" | grep -v "^$" | grep "^_" > "${PLATFORM_LIBS_DIR}/symbols"
+# xcrun nm -gUj "${PLATFORM_LIBS_DIR}/libcrypto.a" | grep -v "^$" | grep "^_" > "${PLATFORM_LIBS_DIR}/symbols"
 
 # Prefixed headers
-mkdir -p "${PLATFORM_LIBS_DIR}/PGPBoringSSL"
-cp ${BORINGSSL_SRC_DIR}/include/openssl/* "${PLATFORM_LIBS_DIR}/PGPBoringSSL"
-go run ${BORINGSSL_SRC_DIR}/util/make_prefix_headers.go -out ${PLATFORM_LIBS_DIR}/PGPBoringSSL "${PLATFORM_LIBS_DIR}/symbols"
-chmod 444 ${PLATFORM_LIBS_DIR}/PGPBoringSSL/*
+mkdir -p "${PLATFORM_LIBS_DIR}/../PGPBoringSSL"
+cp ${BORINGSSL_SRC_DIR}/include/openssl/* "${PLATFORM_LIBS_DIR}/../PGPBoringSSL"
+go run ${BORINGSSL_SRC_DIR}/util/make_prefix_headers.go -out ${PLATFORM_LIBS_DIR}/../PGPBoringSSL "${TMP_DIR}/symbols-iphoneos.txt"
+chmod 444 ${PLATFORM_LIBS_DIR}/../PGPBoringSSL/*
 
-pushd ${PLATFORM_LIBS_DIR}/PGPBoringSSL
+pushd ${PLATFORM_LIBS_DIR}/../PGPBoringSSL
 # Now change the imports from "<PGPBoringSSL/X> to "<SYMBOLS_PREFIX_X>", apply the same prefix to the 'boringssl_prefix_symbols' headers.
-find . -name "*.[ch]" -or -name "*.cc" -or -name "*.S" | xargs gsed -i -e 's+include <openssl/+include <'"${SYMBOLS_PREFIX}"'_+' -e 's+include <boringssl_prefix_symbols+include '"<${SYMBOLS_PREFIX}"'_boringssl_prefix_symbols+'
+# find . -name "*.[ch]" -or -name "*.cc" -or -name "*.S" | xargs gsed -i -e 's+include <openssl/+include <'"${SYMBOLS_PREFIX}"'_+' -e 's+include <boringssl_prefix_symbols+include '"<${SYMBOLS_PREFIX}"'_boringssl_prefix_symbols+'
 # Okay now we need to rename the headers adding the prefix "${SYMBOLS_PREFIX}_".
-find . -name "*.h" | gsed -e "s_./__" | xargs -I {} mv {} "${SYMBOLS_PREFIX}_{}"
+# find . -name "*.h" | gsed -e "s_./__" | xargs -I {} mv {} "${SYMBOLS_PREFIX}_{}"
 # Finally, make sure we refer to them by their prefixed names, and change any includes from angle brackets to quotation marks.
-find . -name "*.h" | xargs gsed -i -e 's/include "/include "'"${SYMBOLS_PREFIX}"'_/' -e 's/include <'"${SYMBOLS_PREFIX}"'_\(.*\)>/include "'"${SYMBOLS_PREFIX}"'_\1"/'
+# find . -name "*.h" | xargs gsed -i -e 's/include "/include "'"${SYMBOLS_PREFIX}"'_/' -e 's/include <'"${SYMBOLS_PREFIX}"'_\(.*\)>/include "'"${SYMBOLS_PREFIX}"'_\1"/'
 # We need BoringSSL to be modularised
 echo "MODULARISING BoringSSL"
 
-cat << EOF > "${PLATFORM_LIBS_DIR}/PGPBoringSSL/PGPBoringSSL.h"
+cat << EOF > "${PLATFORM_LIBS_DIR}/../PGPBoringSSL/PGPBoringSSL.h"
 //===----------------------------------------------------------------------===//
 // This source file is part of the ObjectivePGP project
 //===----------------------------------------------------------------------===//
@@ -208,10 +214,10 @@ cat << EOF > "${PLATFORM_LIBS_DIR}/PGPBoringSSL/PGPBoringSSL.h"
 EOF
 
 for h in ${PGPBORINGSSL_MODULE_HEADERS[@]}; do
-  echo "#include \"$h\"" >> ${PLATFORM_LIBS_DIR}/PGPBoringSSL/PGPBoringSSL.h
+  echo "#include \"$h\"" >> ${PLATFORM_LIBS_DIR}/../PGPBoringSSL/PGPBoringSSL.h
 done
 
-echo  "#endif  // PGP_BORINGSSL_H" >> "${PLATFORM_LIBS_DIR}/PGPBoringSSL/PGPBoringSSL.h"
+echo  "#endif  // PGP_BORINGSSL_H" >> "${PLATFORM_LIBS_DIR}/../PGPBoringSSL/PGPBoringSSL.h"
 popd
 
 rm -rf ${TMP_DIR}
