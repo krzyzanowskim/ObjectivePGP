b108f76fa03e4056e526ccf3a18dbd835b548cf7
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2023-04-27 09:47:53 +0200

    Disable bogus warning

--- ObjectivePGP/CryptoBox/PGPCryptoHash.m
+++ ObjectivePGP/CryptoBox/PGPCryptoHash.m
@@ -69,8 +69,11 @@ NSData *_Nullable PGPCalculateHash(PGPHashAlgorithm algorithm, NS_NOESCAPE PGPUp
         return outData;                                                     \
     }
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
 commonHashImpl(md5, CC_MD5_Init, CC_MD5_Update, CC_MD5_Final, CC_MD5_CTX, CC_MD5_DIGEST_LENGTH);
 commonHashImpl(sha1, CC_SHA1_Init, CC_SHA1_Update, CC_SHA1_Final, CC_SHA1_CTX, CC_SHA1_DIGEST_LENGTH);
+#pragma clang diagnostic pop
 commonHashImpl(sha224, CC_SHA224_Init, CC_SHA224_Update, CC_SHA224_Final, CC_SHA256_CTX, CC_SHA224_DIGEST_LENGTH);;
 commonHashImpl(sha256, CC_SHA256_Init, CC_SHA256_Update, CC_SHA256_Final, CC_SHA256_CTX, CC_SHA256_DIGEST_LENGTH);
 commonHashImpl(sha384, CC_SHA384_Init, CC_SHA384_Update, CC_SHA384_Final, CC_SHA512_CTX, CC_SHA384_DIGEST_LENGTH);
